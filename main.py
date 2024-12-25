fc0dc014a7828344ef6fab5ab51170b4ae0f28db
Yue Yin <yueyin.dev@gmail.com>
2024-12-25 16:54:46 -0500

    Demonstrate constructor arg, sharing variables, and method overriding

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import socketserver
 
 
 class Server(socketserver.ThreadingTCPServer):
+    # How to pass argument to server constructor?
+    # https://stackoverflow.com/a/14133194/9057530
+    def __init__(self, server_address, RequestHandlerClass):
+        socketserver.ThreadingTCPServer.__init__(
+            self, server_address, RequestHandlerClass
+        )
+        self.request_count = 0
+
     def server_activate(self):
         print("Server is being activated!")
         super().server_activate()
+
+    def process_request(self, request, client_address):
+        print(f"Process request {request} from {client_address}")
+        self.request_count += 1
+        super().process_request(request, client_address)
 
     def finish_request(self, request, client_address):
         print(f"Handling request from {client_address}")
@@ -13,8 +26,11 @@ class Server(socketserver.ThreadingTCPServer):
 
 class Handler(socketserver.StreamRequestHandler):
     def handle(self):
-        # self.server refers to the server object
-        print("Handle!")
+        # How to share variables among handlers?
+        # self refers to the handler object and is distinct for each request
+        # self.server refers to the server object and is shared among handlers
+        # https://stackoverflow.com/a/6875827/9057530
+        print(f"Request #{self.server.request_count}")
 
 
 def main():
